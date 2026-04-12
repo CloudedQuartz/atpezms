81e4572da7913722878075388853a69d7b5f8649
Ayan Pande <ayan.pande06@gmail.com>
2026-04-12 10:59:47 +0000

    chore: spring boot project from start.spring.io (unmodified)

---
+++ src/main/java/com/atpezms/atpezms/AtpezmsApplication.java
@@ -0,0 +1,13 @@
+package com.atpezms.atpezms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtpezmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtpezmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atpezms/atpezms/AtpezmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atpezms.atpezms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtpezmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
